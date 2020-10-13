fa3bd461ade540c434d35c313a975bc90191f807
vmo-campos <viniciusmoliveirac@hotmail.com>
2020-10-13 16:49:23 -0300

    Adding person creation function

--- src/functions/createMultiplesPessoas.ts
+++ src/functions/createMultiplesPessoas.ts
@@ -1,1 +1,12 @@
-export default function createMultiplesPessoas() {}
+import Pessoa from "../entities/Pessoa";
+import { IPessoaCreate } from "../dtos/IPessoaCreation";
+
+export default function createMultiplesPessoas(
+  pessoas: IPessoaCreate[]
+): Pessoa[] {
+  const ps: Pessoa[] = [];
+  pessoas.forEach((p) => {
+    ps.push(new Pessoa(p));
+  });
+  return ps;
+}
